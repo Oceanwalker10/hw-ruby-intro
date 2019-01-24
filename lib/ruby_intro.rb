825e2642c1953fd1271fa9b83419a37d542b0542
Ian C <zilin.chen@faulkner.edu>
2019-01-24 05:32:59 +0000

    Complete constructor and check if isbn number is empty or check if the price is less than or equal to zero

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -57,6 +57,7 @@ end
 class BookInStock
     attr_accessor :isbn, :price
     def initialize isbn, price
+        raise ArgumentError if isbn.empty? || price <= 0
         @isbn = isbn
         @price = price
     end
