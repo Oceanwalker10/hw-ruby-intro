9d88a4a627aa2c15a9581887a7cd3902afcd4b19
Ian C <zilin.chen@faulkner.edu>
2019-01-24 05:36:12 +0000

    Construct a method that returns price in proper format

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -56,9 +56,13 @@ end
 
 class BookInStock
     attr_accessor :isbn, :price
-    def initialize isbn, price
+    def initialize(isbn, price)
         raise ArgumentError if isbn.empty? || price <= 0
         @isbn = isbn
         @price = price
+    end
+    
+    def price_as_string
+        format("$%.2f", @price)
     end
 end
